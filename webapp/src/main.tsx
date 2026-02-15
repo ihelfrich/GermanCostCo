690f5509f49361393a28c56de8a019ec3ae199f0
ihelfrich <60946155+ihelfrich@users.noreply.github.com>
2026-02-15 14:31:15 -0600

    Use HashRouter for GitHub Pages route compatibility

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import App from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
